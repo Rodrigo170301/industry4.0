7c32f84ca1e6c9002e2d4c7936c5761666d4b9bb
campos <37480718+Rodrigo170301@users.noreply.github.com>
2020-03-01 22:54:50 -0600

    Add files via upload
    the first parctice that que made

---
+++ Activity_01.py
@@ -0,0 +1,10 @@
+name="Axel"
+age='18'
+mayor='TI'
+term='1'
+active='true'
+print('name:{}'.format('Axel'))
+print('age:{}'.format('18 years'))
+print('mayor:{}'.format('TI'))
+print('term:{}'.format('1'))
+print('active:{}'.format('true'))
