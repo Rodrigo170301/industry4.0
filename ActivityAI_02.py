a53f8bb9f68e69297688cb0d935ea52e46edb928
campos <37480718+Rodrigo170301@users.noreply.github.com>
2020-03-04 23:25:23 -0600

    Add files via upload

---
+++ ActivityAI_02.py
@@ -0,0 +1,39 @@
+username = input('Introduce your name: ')
+print( f'Greetings, {username}')
+s_age = input('Age:')
+age = int(s_age)
+s_state  = input('married or free')
+print(f'So you are {age} years old and {s_state}interesting to say the least.')
+
+print("told me about you.")
+You_gender=input('Your are a male or female (m or f)?')
+if You_gender != '':
+	if You_gender== 'M':
+		gender='Male'
+	if You_gender=='F':
+		gender='Female'
+	else:
+		print(f'gmm ({You_gender})are you stupid or something like that? ')
+
+You_fb=input('tell me wich is your fb name ?')
+if You_fb != '':
+	fb=You_fb
+
+	answer=input( 'hey can you give me your phone number? (y/n)')
+	if answer=='y':
+		phone_number = input( 'Phone number:' )
+
+	answer = input('awesome now you win an iphone x you want to coniue? (y/n)')
+	if answer== 'y':
+		credit_card=input( 'you only have to tell me your credit card number:')
+
+print('well you are so inteligente you win an iphone x:')
+
+
+if fb != '':
+    print(f'Email: {fb}')
+if phone_number != '':
+    print(f'Phone number: {phone_number}')
+if credit_card != '':
+    print(f'credit card: {credit_card}')
+	print('came on teacher this is a very good code and is fony and you know wath you win an iphone X wuuuuu:')
